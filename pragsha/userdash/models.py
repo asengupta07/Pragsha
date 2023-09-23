6e28180b292412b0475e7c31d635abdd07a70a18
pradyumna-bhowmick <pradyumna.bhowmick.2016@gmail.com>
2023-09-23 12:39:15 +0530

    new user

--- pragsha/userdash/models.py
+++ pragsha/userdash/models.py
@@ -4,7 +4,8 @@ from authentification.models import User
 
 class Broadcast(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+    name = models.TextField()
+    latitude = models.FloatField(max_length=200)
+    longitude=models.FloatField(max_length=200)
     timestamp = models.DateTimeField(default=timezone.now)
     is_accepted = models.BooleanField(default=False)
