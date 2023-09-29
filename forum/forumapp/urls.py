88fbe6407df46a430a1d0a8b51d73f5a4a62a6fd
Arnab Sengupta <136733971+asengupta07@users.noreply.github.com>
2023-09-29 22:17:37 +0530

    Deleted forum directory

--- forum/forumapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.forum, name='forumapp'),
-    path('create_thread/', views.create_thread, name='create_thread'),
-    path('thread_detail/', views.thread_detail, name='thread_detail'),
-]
