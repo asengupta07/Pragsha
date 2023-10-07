06f3ffa5dc1f65351c5f411f8878d526f2189f2c
Arnab S <arnabsengupta104@gmail.com>
2023-10-07 11:09:35 +0530

    Created faq page

--- pragsha/land/urls.py
+++ pragsha/land/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'land'
 urlpatterns = [
     path('', views.landing, name='home'),
+    path('faq/', views.faq, name='faq'),
 ]
--- pragsha/land/views.py
+++ pragsha/land/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def landing(request):
     return render(request, 'land/home.html')
+
+def faq(request):
+    return render(request, 'land/faq.html')
