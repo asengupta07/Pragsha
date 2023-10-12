7378fe2e9e90385a6231d700deb7e970b62e30eb
Kanishk <kanishkpaul23@gmail.com>
2023-10-12 15:43:29 +0530

    removed quora

--- pragsha/quora/urls.py
+++
@@ -1,8 +0,0 @@
-# quora_app/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('question_list/', views.question_list, name='question_list'),
-    path('question_detail/', views.question_detail, name='question_detail'),
-]
